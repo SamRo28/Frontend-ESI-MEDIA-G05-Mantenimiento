0f84019c710045ee637b3f2e674a8f5d7b32b6bd
SamRo28 <Samuel.Rodero@alu.uclm.es>
2025-11-28 09:53:19 +0100

    Agrego proyecto movil

---
+++ FrontendMovil/esi-media/android/app/src/main/java/com/esimediag05Mantenimiento/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.esimediag05Mantenimiento.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ FrontendMovil/esi-media/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.esimediag05Mantenimiento.app',
+  appName: 'esimedia-mantenimiento',
+  webDir: 'dist/EsiMediaFE/browser'
+};
+
+export default config;
